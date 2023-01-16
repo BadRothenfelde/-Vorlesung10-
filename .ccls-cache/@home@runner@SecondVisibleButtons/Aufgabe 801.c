0d8bf6d9bc35069c78447cd885b99fa7cf59aa35
Dan <116747618+BadRothenfelde@users.noreply.github.com>
2023-01-16 15:00:28 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@SecondVisibleButtons/Aufgabe 801.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<string.h>
+
+int main () {
+  printf("Gebe eine Zahl ein (Zahl ist ein Produkt aus der Produktpalette.) von 1-4\n");
+  int zahl; 
+  scanf("%d", &zahl);
+  
+  //Produktname
+  char* Produktname [4];
+  Produktname [0] = "Twix"; 
+  Produktname [1] = "Mars"; 
+  Produktname [2] = "Snickers"; 
+  Produktname [3] = "Bounty";  
+
+  //Packungsgröße in Gramm
+  float Packungsgröße [4]; 
+  Packungsgröße [0] = 200; 
+  Packungsgröße [1] = 350; 
+  Packungsgröße [2] = 300;
+  Packungsgröße [3] = 150; 
+
+ // Preis in Euro
+  float Preis [4]; 
+  Preis [0] = 2; 
+  Preis [1] = 2.5;
+  Preis [2] = 2.6; 
+  Preis [3] = 2.9; 
+
+// Preis pro Kg (netto)
+int preisprokgnetto;
+preisprokgnetto = Preis [zahl-1] / (Packungsgröße [zahl-1] / 1000); 
+
+//Preis pro Kg (brutto)
+int preisprokgbrutto; 
+  preisprokgbrutto = Preis [zahl-1] / (Packungsgröße [zahl-1] / 1000) * 1.07; 
+
+printf("%s %f %d %d", Produktname[zahl-1], Preis [zahl-1], preisprokgnetto, preisprokgbrutto);
+
+return 0; 
+}
